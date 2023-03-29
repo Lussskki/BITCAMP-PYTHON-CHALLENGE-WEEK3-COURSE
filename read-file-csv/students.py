22445319ea0458a16d255ba04f92a3b8101abfed
Luka Guledani <monster.striker21@gmail.com>
2023-03-29 19:01:55 +0400

    week3-file managment i/o

--- read-file-csv/students.py
+++ read-file-csv/students.py
@@ -2,4 +2,5 @@ with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
         # print(row)
-        print(f"{row[0]} is in {row[1]}")
+        print(f"{row[0]} is in {row[1]}") 
+                 
